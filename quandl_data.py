b6f18efe8b31579e3b1db185f630a6bccc94c134
Juan Ignacio Gil Gómez <juanignacio@gilgomez.net>
2017-10-01 19:58:05 +0200

    Test with ta-lib

--- quandl_data.py
+++ quandl_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+import talib
 import quandl
 quandl.ApiConfig.api_key = 'nW629kYKyDBPBFq1fuHs'
 
@@ -18,6 +19,50 @@ SP100 = ['AA', 'AAPL', 'ABT', 'AEP', 'AMGN', 'AMZN', 'AVP', 'AXP',
 
 
 def get_data_from_quandl(file='data.csv'):
-        data = quandl.get_table('WIKI/PRICES', ticker=SP100, date={'gte': '2000-01-01'}, paginate=True)
+    """Example function with PEP 484 type annotations.
+
+    Args:
+        param1: The first parameter.
+        param2: The second parameter.
+
+    Returns:
+        The return value. True for success, False otherwise.
+
+    """
+
+    data = quandl.get_table('WIKI/PRICES', ticker=SP100, date={'gte': '2000-01-01'}, paginate=True)
+
+    if file:
         data.to_csv(file)
-        return data
+
+    return data
+
+
+def add_technical_indicators(data=None, csv_file='data.csv'):
+    """Example function with PEP 484 type annotations.
+
+    Args:
+        data (pandas.DataFrame): DataFrame with option prices.
+        param2: The second parameter.
+
+    Returns:
+        data (pandas.DataFrame) : DataFrame with data with technical indicators added
+
+    """
+
+    if csv_file:
+        data = pd.read_csv(csv_file, index_col=0)
+
+    if data is None:
+        raise KeyError('You must provide data or csv_file')
+
+    return data
+
+
+if __name__ == "__main__":
+
+    data = add_technical_indicators(csv_file='data.csv')
+    data = data[data['ticker'] == 'AA']
+    macd, macdsignal, macdhist = talib.MACD(data['close'].as_matrix(), fastperiod=12, slowperiod=26, signalperiod=9)
+    print(macd)
+
